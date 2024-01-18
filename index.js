6d93581e911c7d8776a8da399b8b4811d388e1b2
c0mradeXZTY <mavedimavela@gmail.com>
2024-01-18 06:20:46 -0800

    Done with assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = () => {
+   let num1 = 2000
+   let num2 = 100
+   return num1 / num2;
+}
+
+let square = (element) => {
+ return element * element
+}
+
+let add = () => {
+    let a = 3
+    let b = 4
+    return a + b;
+}
